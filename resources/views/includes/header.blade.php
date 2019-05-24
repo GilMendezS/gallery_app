1ef3e59650c4549fe97409a422418eaae97573f2
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-05-24 12:56:42 -0500

    colors modified

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,67 +1,25 @@
-<header class="site-navbar py-3" role="banner">
-
-    <div class="container-fluid">
-      <div class="row align-items-center">
-        
-        <div class="col-6 col-xl-2" data-aos="fade-down">
-          <h1 class="mb-0"><a href="{{url('/')}}" class="text-black h2 mb-0">Cuadros Bethel Montejo</a></h1>
-        </div>
-        <div class="col-10 col-md-8 d-none d-xl-block" data-aos="fade-down">
-          <nav class="site-navigation position-relative text-right text-lg-center" role="navigation">
-
-            <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
-              <li class="{{ Request::path() == '/' ? 'active' : '' }}"><a href="{{url('/')}}">Home</a></li>
-              <!--li class="has-children">
-                <a href="single.html">Gallery</a>
-                <ul class="dropdown">
-                  <li><a href="#">Nature</a></li>
-                  <li><a href="#">Portrait</a></li>
-                  <li><a href="#">People</a></li>
-                  <li><a href="#">Architecture</a></li>
-                  <li><a href="#">Animals</a></li>
-                  <li><a href="#">Sports</a></li>
-                  <li><a href="#">Travel</a></li>
-                  <li class="has-children">
-                    <a href="#">Sub Menu</a>
-                    <ul class="dropdown">
-                      <li><a href="#">Menu One</a></li>
-                      <li><a href="#">Menu Two</a></li>
-                      <li><a href="#">Menu Three</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </li-->
-              <!--li><a href="services.html">Services</a></li-->
-              <li><a href="{{route('gallery')}}">Galería</a></li>
-              <li class="{{ Request::path() == 'about' ? 'active' : '' }}"><a href="{{route('about')}}">Acerca de</a></li>
-              <li class="{{ Request::path() == 'contact' ? 'active' : '' }}"><a href="{{route('contact')}}">Contacto</a></li>
-            </ul>
-          </nav>
-        </div>
-
-        <div class="col-6 col-xl-2 text-right" data-aos="fade-down">
-          <div class="d-none d-xl-inline-block">
-            <ul class="site-menu js-clone-nav ml-auto list-unstyled d-flex text-right mb-0" data-class="social">
-              <li>
-                <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-              </li>
-              <li>
-                <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-              </li>
-              <li>
-                <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-              </li>
-              <li>
-                <a href="#" class="pl-3 pr-3"><span class="icon-youtube-play"></span></a>
-              </li>
-            </ul>
-          </div>
-
-          <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
-        </div>
-
-      </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <a class="navbar-brand" href="{{url('/')}}">
+      <img src="{{asset('/images/logo.jpeg')}}" width="100" height="30" alt="Cuadros Bethel">
+      Cuadros Bethel Montejo
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item {{ Request::path() == '/' ? 'active' : '' }}">
+          <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item {{ Request::path() == '/gallery' ? 'active' : '' }}">
+          <a class="nav-link" href="{{route('gallery')}}">Galería</a>
+        </li>
+        <li class="nav-item {{ Request::path() == '/about' ? 'active' : '' }}">
+          <a class="nav-link" href="{{route('about')}}">Acerca de</a>
+        </li>
+        <li class="nav-item {{ Request::path() == '/contact' ? 'active' : '' }}">
+          <a class="nav-link " href="{{route('contact')}}">Contacto</a>
+        </li>
+      </ul>
     </div>
-    
-  </header>
+  </nav>
