3dbb8b43da4c735b990cdfd31eadf863badaa6b3
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-05-24 17:16:31 -0500

    zoom modified

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -24,7 +24,7 @@
         var uluru = {lat: 16.7303688, lng:  -93.0920443};
         // The map, centered at Uluru
         var map = new google.maps.Map(
-            document.getElementById('map'), {zoom: 4, center: uluru});
+            document.getElementById('map'), {zoom: 7, center: uluru});
         // The marker, positioned at Uluru
         var marker = new google.maps.Marker({position: uluru, map: map});
       }
